3b49588c6ce565bc250b9a50efaa1347923a8b36
Christopher Scott <cscottdev@protonmail.com>
2024-04-29 17:46:50 -0500

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,9 @@ import settings  from './settings.json' assert { type : 'json' }
 
 const presets = Object.keys(templates)
 
-const preset_default  = templates[presets[0] as keyof typeof templates]
-const default_session = DraftUtil.create(preset_default.proposal as ProposalData, preset_default.roles as RoleTemplate[])
+const preset_default   = templates[presets[0] as keyof typeof templates]
+const proposal_default = preset_default.proposal as ProposalData
+const roles_default    = preset_default.roles as RoleTemplate[]
+const default_session  = DraftUtil.create(proposal_default, roles_default)
 
 export default { default_session, presets, servers, settings, templates }
